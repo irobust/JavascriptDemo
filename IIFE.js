08493280d23a91c852b245022784f85e1f2ccf45
Phanupong <phanupong.pe@gmail.com>
2016-01-14 13:28:41 +0700

    First Time Commit

---
+++ IIFE.js
@@ -0,0 +1,5 @@
+(function ($, window){
+	var a = 20;
+})(jQuery, window);
+
+console.log(a);
---
+++ Syntax.js
@@ -0,0 +1,72 @@
+// IF-ELSE
+
+var a;
+
+if(!!a){
+	// true
+}else{
+	// false
+}
+
+if(condition){
+	
+}else if(condition){
+	
+}else if(condition){
+	
+}else{
+	
+}
+
+// && || == === != !==
+
+
+// FOR LOOP
+for (var i = arguments.length - 1; i >= 0; i--) {
+	arguments[i]
+};
+
+for(var i = 0; i < 10; i++ ){
+
+}
+
+for(var i in things){
+	things[i]
+}
+
+// Other loop
+var i=0;
+while(i < 10){
+
+	i++;
+}
+
+var i=0;
+do{
+	i++;
+}while(i < 10);
+
+//switch
+
+switch(ans){
+	case 'a':
+				statements_1
+				break;
+	case 'b':
+				statements_2
+				break;
+
+	case 'c':
+	case 'd':
+				statements_3
+				break;
+	default:
+		statements_def
+		break;
+}
+
+
+
+
+
+
